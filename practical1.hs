0a102ffcfcd4651e2f44d5e9aafc968a46436131
Paulius Imbrasas <pi514@york.ac.uk>
2017-04-13 14:24:22 +0100

    More functions done

--- practical1.hs
+++ practical1.hs
@@ -1,6 +1,7 @@
 module Practical1 where
 
 import Prelude hiding (take, drop, zipWith)
+import Debug.Trace (traceShow)
 
 take, drop :: Int -> [a] -> [a]
 take _ [] = []
@@ -43,3 +44,41 @@ instance Show a => Show (Tri a) where
 		      row _ [] = []
 		      pad n = replicate (length rs - (n + 1)) ' '
 		      f r = unwords $ map show r
+			  
+trol :: Tri a -> Tri a
+trol (Tri rs) = Tri (reverse $ f rs)
+	where f rs | null . last $ rs = []
+	      f rs = let (e', rs') = unzip [(e, rs) | (e:rs) <- rs] in e' : f rs' 
+		  
+tror :: Tri a -> Tri a		  
+tror (Tri rs) = Tri (reverse . f . reverse $ rs)
+	where f rs | null . head $ rs = []
+	      f rs = let (e', rs') = unzip [(e, rs) | (e:rs) <- rs] in e' : f rs' 
+		  
+sublists :: [a] -> [[a]]
+sublists [] = [[]]
+sublists xs = inner xs (length xs) ++ sublists (drop 1 xs)
+	where 
+		inner _ 0  = []
+		inner xs n = take n xs : inner xs (pred n)
+		
+prefixes :: [a] -> [[a]]
+prefixes [] = []
+prefixes xs = f 1
+	where f n | n == length xs + 1 = []
+	          | otherwise      = take n xs : f (succ n)
+
+suffixes :: [a] -> [[a]]
+suffixes = reverse . prefixes
+
+segments :: [a] -> [[a]]
+segments xs = [xs' | xs' <- sublists xs, not . null $ xs']
+
+perms :: [a] -> [[a]]
+perms xs = [xs] -- todo
+
+parts :: [a] -> [[[a]]]
+parts xs = [[xs]] -- todo
+
+change :: [Int] -> Int -> [[Int]]
+change cs m = [xs]
